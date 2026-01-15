2193e394ce20b7e4a92e8a82ea0baf33a5a9e131
Michael Sosa <micaelsosa2@gmail.com>
2026-01-14 19:20:42 -0500

    Change database provider from SQLite to PostgreSQL in schema.prisma and migration_lock.toml. Add initial migration SQL for User and Job tables with constraints and indexes.

--- prisma/migrations/20260115002006_init/migration.sql
+++ prisma/migrations/20260115002006_init/migration.sql
@@ -1,17 +1,19 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "name" TEXT,
     "password" TEXT,
     "googleId" TEXT,
     "image" TEXT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Job" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "visaEmail" TEXT NOT NULL,
     "visaPassword" TEXT NOT NULL,
@@ -21,14 +23,15 @@ CREATE TABLE "Job" (
     "notEarlierThan" TEXT,
     "status" TEXT NOT NULL,
     "processId" INTEGER,
-    "lastCheck" DATETIME,
+    "lastCheck" TIMESTAMP(3),
     "lastMessage" TEXT,
     "foundDates" TEXT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
     "successDate" TEXT,
     "successTime" TEXT,
-    CONSTRAINT "Job_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+
+    CONSTRAINT "Job_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -36,3 +39,6 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_googleId_key" ON "User"("googleId");
+
+-- AddForeignKey
+ALTER TABLE "Job" ADD CONSTRAINT "Job_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
